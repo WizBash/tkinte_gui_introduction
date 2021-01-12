9933f2b2cf4d6ae183a05a9cc93d6484dab9ba06
WizBash <53521065+WizBash@users.noreply.github.com>
2021-01-11 21:24:02 -0800

    Update login.py

--- login.py
+++ login.py
@@ -9,6 +9,13 @@ from tkinert import ttk
 
 root=Tk()
 root.title('My Login Form')
+root.config(bg='green')
+
+#=====================starting widgets=======================
+frame1=Frame(root,bg='lightblue')
+frame1.grid(row=1,column=1,columnspan=3,sticky='nw')
+l1=Label(frame1,text='My Log In Form',bg='lightblue',fg='green',font=('times',15))
+l1.grid(row=1,column=1,padx=10,pady=10)
 
 
 mainloop()
